a970f1d282111ae164cfa00e680ba30bbf88ee6b
felipe w <callofnoob712@gmail.com>
2026-04-23 19:45:06 -0300

    Rotas para navegação e fluxo de telas

---
+++ src/navigation/routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "../screens/home";
+import React from "react";
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export default function Routes() {
+	return (
+		<NavigationContainer>
+			<Navigator initialRouteName="Home">
+				<Screen name="Home" component={Home} />
+			</Navigator>
+		</NavigationContainer>
+	);
+}
