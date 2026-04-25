47daf0a889b26dfad78d8f5e6a800531ccc78152
felipe w <callofnoob712@gmail.com>
2026-04-25 15:16:50 -0300

    Novas configurações para padrão de importação

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+	presets: ['module:@react-native/babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./'],
+				extensions: ['.ts', '.tsx', '.js', '.json'],
+				alias: {
+					'@components': './src/components',
+				},
+			},
+		],
+	],
 };
