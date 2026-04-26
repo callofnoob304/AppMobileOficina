084bf19dbda12b7220e4b7337cbfaf96435e4201
felipe w <callofnoob712@gmail.com>
2026-04-26 13:20:58 -0300

    Tela de histórico

---
+++ src/screens/historico/index.tsx
@@ -0,0 +1,10 @@
+import { Container, Label } from "@components";
+import React from "react";
+
+export default function Historico() {
+	return (
+		<Container>
+			<Label title={"Tela histórico"} />
+		</Container>
+	);
+}
