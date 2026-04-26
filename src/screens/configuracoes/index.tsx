acb491e196c20aabe6a870b84495a486f2581514
felipe w <callofnoob712@gmail.com>
2026-04-26 13:20:52 -0300

    Tela de configurações

---
+++ src/screens/configuracoes/index.tsx
@@ -0,0 +1,10 @@
+import { Container, Label } from "@components";
+import React from "react";
+
+export default function Configuracoes() {
+	return (
+		<Container>
+			<Label title={"Tela Configurações"} />
+		</Container>
+	);
+}
